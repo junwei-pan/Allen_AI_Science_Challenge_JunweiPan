07d3a7bf6de79db972c07e2c375c28fe7196d5f5
Junwei Pan <jwpan@admb1044.yse.corp.yahoo.com>
2015-10-11 05:41:40 +0000

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -3,7 +3,7 @@ from urllib2 import urlopen
 #from BeautifulSoup import BeautifulSoup as BS
 from bs4 import BeautifulSoup
 
-def get_lesson_url_from_topic_url(url_topic):
+def get_url_lesson_from_url_topic(url_topic):
     # Topic includes: Earth Science, Life Science, Physical Science, Biology, Chemestry and Physics
     lst_url = []
     html = urlopen(url_topic).read()
@@ -13,7 +13,7 @@ def get_lesson_url_from_topic_url(url_topic):
         lst_url.append(url_topic + r_url.strip('/').split('/')[1])
     return lst_url
 
-def get_read_url_from_lesson_url(url_lesson):
+def get_read_url_from_url_lesson(url_lesson):
     s_lesson = url_lesson.strip('/').split('/')[-1]
     return "%s/lesson/%s/?referrer=content_details" % (url_lesson, s_lesson)
  
@@ -23,13 +23,30 @@ def crawl_text_from_read_url(url_read):
     soup = BeautifulSoup(html, 'html.parser')
     print soup.find_all('div', id = 'modality_content')
     
+def get_keyword_from_url_topic(url_topic):
+    # Topic includes: Earth Science, Life Science, Physical Science, Biology, Chemestry and Physics
+    lst_url = []
+    html = urlopen(url_topic).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    for tag_h3 in soup.find_all('h3'):
+        url_res =  ' '.join(tag_h3.li.a.get('href').strip('/').split('/')[-1].split('-'))
+        lst_url.append(url_res)
+    return lst_url
 
-url_topic_earth = 'https://www.ck12.org/earth-science/'
+lst_url_topic = ['https://www.ck12.org/earth-science/', 'http://www.ck12.org/life-science/', 'http://www.ck12.org/physical-science/', 'http://www.ck12.org/biology/', 'http://www.ck12.org/chemistry/', 'http://www.ck12.org/physics/']
+
+set_keyword = set()
+for url_topic in lst_url_topic:
+    lst_keyword = get_keyword_from_url_topic(url_topic)
+    for keyword in lst_keyword:
+        set_keyword.add(keyword)
+for keyword in set_keyword:
+    print keyword
 '''
-for url in get_lesson_url_from_topic_url(url_topic_earth):
+for url in get_url_lesson_from_url_topic(url_topic_earth):
     print url
-    get_read_url_from_lesson_url(url)
+    get_read_url_from_url_lesson(url)
 '''
-url_topic = 'https://www.ck12.org/earth-science/Development-of-Hypotheses'
-url_read =  get_read_url_from_lesson_url(url_topic)
-crawl_text_from_read_url(url_read)
+#url_topic = 'https://www.ck12.org/earth-science/Development-of-Hypotheses'
+#url_read =  get_read_url_from_url_lesson(url_topic)
+#crawl_text_from_read_url(url_read)
